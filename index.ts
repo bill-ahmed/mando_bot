11e399d1a2cdff9cdff46e7363c4a80487e417dd
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-01-14 17:09:54 -0500

    initial setup, template for config

--- index.ts
+++ index.ts
@@ -1,16 +1,17 @@
-// const { App } = require('@slack/bolt');
+const { App } = require('@slack/bolt');
 
-// const app = new App({
-//   signingSecret: process.env.SLACK_SIGNING_SECRET,
-//   token: process.env.SLACK_BOT_TOKEN,
-// });
+const config = require('./config.json');
 
-// /* Add functionality here */
+const app = new App({
+  signingSecret: config.slack.signing_secret,
+  token: config.slack.bot_token,
+});
 
-// (async () => {
-//   // Start the app
-//   await app.start(process.env.PORT || 3000);
+/* Add functionality here */
 
-//   console.log('⚡️ Bolt app is running!');
-// })();
-console.log("hello");
+(async () => {
+  // Start the app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
