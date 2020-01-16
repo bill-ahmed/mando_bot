d4f5b79bdfa9e82098772e784a892027865910b8
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-01-15 19:16:48 -0500

    remove bolt, use custom implementation

--- index.ts
+++ index.ts
@@ -1,29 +1,30 @@
-const { App } = require('@slack/bolt');
+const config = require('./config.json');
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser')
 
-const PORT = 3500;                        // Use port 2500
-const config = require('./config.json');  // Load config info
+const port = 3500
+const ALLOW_TOKEN = config.slack.slack_token;
 
-const app = new App({
-  signingSecret: config.slack.signing_secret,
-  token: config.slack.bot_token,
+app.use(bodyParser.json());
+
+/**All normal POST requests through here./*
+app.get('/', (req: any, res: any) => {
+	if(req.body.token === ALLOW_TOKEN){
+		console.log("Good request", req.body);
+	} else {
+		console.err("Recieved invalid token, rejecting request...");
+	}
 });
 
-app.error((err: any) => {
-  console.error(err);
+/**Events endpoint validation*/
+app.post('/slack/events', (req: any, res: any) => {
+	if(req.body.token === ALLOW_TOKEN){
+		console.log("Good request.", req.body);
+		res.sendStatus(200);
+	} else {
+		console.err("Error: Invalid token recieved");
+	}
 });
 
-/* Add functionality here */
-app.message('test', async ({ message, say }: any) => {
-  console.log(message);
-  say(`Hello, <@${message.user}>`);
-});
-
-
-
-
-(async () => {
-  // Start the app
-  await app.start(PORT);
-
-  console.log(`⚡️  Bolt app is running on: http://localhost:${PORT} ⚡️`);
-})();
+app.listen(port, () => console.log(`Mando Bot listening on port ${port}!`))
